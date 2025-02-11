7bf8ebe325d4689ec0e0dd7ebbbd3143ed68d3b9
Giordano Lanna <giordanoal2003@gmail.com>
2025-02-10 22:20:01 -0300

    Increment

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,12 +1,18 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import en from './locales/en.json';
-import pt from './locales/pt.json';
+
+import enTranslations from '../locales/en.json';
+import ptTranslations from '../locales/pt.json';
 
 i18n.use(initReactI18next).init({
+  
   resources: {
-    en: { translation: en },
-    pt: { translation: pt },
+    en: { 
+      ...enTranslations
+    },
+    pt: { 
+      ...ptTranslations
+    },
   },
   lng: 'pt',              // idioma inicial
   fallbackLng: 'pt',
