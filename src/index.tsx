429ab12d79587221795cb0a81c0a73555d972e32
Giuseppe Lanna <giuseppe@orium.network>
2025-02-14 11:10:51 -0300

    fix build not working

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './Login/components/App';
 import './index.css';
 import './lib/i18n';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+const root = document.getElementById('root')!
+createRoot(root).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>
+)
