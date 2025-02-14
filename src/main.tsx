93bf23d63d2ef9ab075d9937cfd7291633a508c8
Giuseppe Lanna <giuseppe@orium.network>
2025-02-14 11:59:02 -0300

    add tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './Login/components/App.tsx'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { routeTree } from './route-tree.gen'
 import './index.css'
-
 import './lib/i18n'
 
+const route = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof route
+  }
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={route} />
   </StrictMode>,
 )
